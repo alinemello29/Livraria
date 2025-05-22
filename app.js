6582a9136428a44cf2fe0c8df67cce9d4e31513e
Aline Melo <109696840+alinemello29@users.noreply.github.com>
2025-05-21 22:24:52 -0300

    Ajuste

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 // Middlewares
 app.use(cors());
